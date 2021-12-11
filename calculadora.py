def161123e6cbcc8f0324f92be426495651f55ce
Thallis Stersi <ts191995@alunos.unisanta.br>
2021-12-11 13:56:00 -0300

    começando a usar a função lower

--- calculadora.py
+++ calculadora.py
@@ -5,44 +5,47 @@ nome = str(input('BEM VINDO A CALCULADORA BOLADA, PRESSIONE [C] PARA CALCULADORA
 tabuada = 0
 
 
+
 while True:
 
-    if nome == 'T' or nome == 't':
+    if nome.lower() == 't':
         numero = int(input("Insira o número: "))
         for i in range(10):
             tabuada += 1
             print(numero * tabuada)
 
 
-    if nome == 'c' or nome == 'C':
+    if nome.lower() == 'c':
         num1 = (input("Digite o primeiro número: "))
         num2 = (input("Digite o segundo número: "))
         operador = (input("Digite o operador: "))
 
 
-    if num1.isdigit() or num2.isdigit():
-        num1 = int(num1)
-        num2 = int(num2)
-    else:
-        print("Digite um número!!!!")
+        if num1.isdigit() or num2.isdigit():
+            num1 = int(num1)
+            num2 = int(num2)
+        else:
+            print("Digite um número!!!!")
 
-    if operador == '+':
-        print(num1 + num2)
-    elif operador == '-':
-        print(num1 - num2)
-    elif operador == '/':
-        print(num1 / num2)
-    elif operador == '*':
-        print(num1 * num2)
-    elif operador == '**':
-        print(num1 ** num2)
-    else:
-        print("Insira um operador válido.")
+        #A = ['+': +, '-': -]
+       
+        if operador == '+':
+            print(num1 + num2)
+        elif operador == '-':
+            print(num1 - num2)
+        elif operador == '/':
+            print(num1 / num2)
+        elif operador == '*':
+            print(num1 * num2)
+        elif operador == '**':
+            print(num1 ** num2)
+        else:
+            print("Insira um operador válido.")
 
 
     sair = input("Digite [N] para sair ou [S] para continuar")
 
-    if sair == 's' or sair == 'S':
+    if sair.lower() == 's':
         nome = str(input('PRESSIONE [C] PARA CALCULADORA OU [T] PARA TABUADA: '))
     else:
         break
