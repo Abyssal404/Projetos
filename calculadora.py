195f79af96771e02811d1405f8273184b9a63a68
Thallis Stersi <ts191995@alunos.unisanta.br>
2021-12-12 20:12:45 -0300

    Novas funções adicionadas

--- calculadora.py
+++ calculadora.py
@@ -1,22 +1,50 @@
 
 def inicio():
-    nome = str(input('BEM VINDO A CALCULADORA BOLADA, PRESSIONE [C] PARA CALCULADORA OU [T] PARA TABUADA: '))
+    nome = str(input('BEM VINDO A CALCULADORA, PRESSIONE [C] PARA CALCULADORA, [T] PARA TABUADA, [O] PARA SABER SE O NÚMERO É ÍMPAR OU PAR OU [F] PARA CALCULAR FATORIAL: '))
+    
+
     if nome.lower() == 'c':
         calculadora()
     if nome.lower() == 't':
         tabuadaaa()
+    if nome.lower() == 'o':
+        ImparPar()
+    if nome.lower() == 'f':
+        fatorial()
+    
+
+
+def fatorial():
+    n = int(input("Digite o valor: "))
+    resultado=1
+    for n in range(1,n+1):
+        resultado *= n
+        print(resultado)
+
+
+def ImparPar():
+    numero = int(input("Digite um número: "))
+
+    if numero % 2 == 0:
+        print("é par!")
+    else:
+        print("é ímpar!")  
+
+
+def creditos():
+     saas = 'https://www.instagram.com/offstersi/'
+     print('MADE BY: {0}'.format(saas))
+   
 
 
 
 def main():
     print('##############################################################')
-    print('MADE BY @offstersi')
     print('USE [**], [*], [-], [+], [/] COMO OPERADORES!')
     inicio()
-    sair()
+    sairr()
     
-
-
+    
        
 def tabuadaaa():
     tabuada = 0
@@ -55,11 +83,16 @@ def calculadora():
         print("Insira um operador válido.")
 
     
-def sair():
-    sair = input("Digite [N] para sair ou [S] para continuar")
+def sairr():
+    sair = input("Digite [N] para sair, [S] para continuar ou [Y] para ir aos créditos: ")
     if sair.upper() == 'S':
         main()
-        
+
+    if sair.upper() == 'Y':
+        creditos()
+        main()
+
+    
 
 if __name__ == "__main__":
     main()
