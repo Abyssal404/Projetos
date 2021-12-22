231d724eef53c62dec518ed8b71c3f8d2940ccb1
Thallis Stersi <ts191995@alunos.unisanta.br>
2021-12-22 18:56:48 -0300

    Pedra, papel, tesoura melhorado.

--- projetoPedra_Papel_Tesoura.py
+++ projetoPedra_Papel_Tesoura.py
@@ -1,61 +1,57 @@
 from random import choice, random
 
+jogadas = ['PEDRA', 'PAPEL', 'TESOURA']
+ganha = 'Você ganhou!'
+jogadawin = [['PAPEL', 'PEDRA'], ['PEDRA', 'TESOURA'], ['TESOURA', 'PAPEL']]
+vitorias = 0
 
 def main():
     a = input("Bem vindo ao pedra, papel, tesoura. Deseja jogar? ")
     if a.lower() == 'sim':
-        jogador1()
-        jogador2()
-        ganhou_perdeu()
+        jogadap1 = jogador1()
+        chutebot = jogador2()
+        ganhou_perdeu(jogadap1, chutebot)
+    else:
+        print(":(")
     
         
 
-
 def jogador1():
     print("UTILIZE [PEDRA], [PAPEL] OU [TESOURA]")
-    global jogada 
-    jogada = str(input("faça sua jogada: "))
-
-    if jogada.lower() == 'pedra':
-        print("Você escolheu Pedra!")
-    elif jogada.lower() == 'tesoura':
-        print("Você escolheu Tesoura!")
-    elif jogada.lower() == 'papel':
-        print("Você escolheu Papel!")
+    jogada = str(input("faça sua jogada: ")).lower()
+    if jogada.upper() in jogadas:
+        print("Você escolheu: {0}!".format(jogada))
+        return jogada
     else:
         print("Digite um valor válido")
     
 
 
 def jogador2(): #AQUI É SÓ UM BOT
-    global chute
-    nomes = ['Pedra!', 'Papel!', 'Tesoura!']
-    chute = choice(nomes)
-
+    chute = choice(jogadas).lower()
     print("O bot escolheu: {0}".format(chute))
+    return chute
 
 
 
-def ganhou_perdeu():
-    if jogada == 'papel' and chute == 'Pedra!':
-        print("Você Ganhou!")
-    elif jogada == 'pedra' and chute == 'Papel!':
-        print('Você perdeu!')
-    elif jogada == 'tesoura' and chute == 'Pedra!':
-        print("Você perdeu!")
-    elif jogada == 'pedra' and chute == 'Tesoura!':
-        print("Você ganhou!")
-    elif jogada == 'papel' and chute == 'Tesoura!':
-        print("Você perdeu!")
-    elif jogada == 'tesoura' and chute == 'Papel!':
-        print("Você ganhou!")
-    else:
+def ganhou_perdeu(jogadap1, chutebot):
+    global vitorias
+    vitorias += 1
+    
+    if([jogadap1.upper(), chutebot.upper()] in jogadawin):
+        print(ganha)
+        print("%i Pontos"%vitorias)
+    elif(jogadap1.upper() == chutebot.upper()):
         print("Empate!")
+    else:
+        print("Você Perdeu!")
+    
+    main()
+    
+    
     
     
 
-
-    
 
 
 
